ff132b2542d12ddd1d5ba1435c7850d3df83e9ba
Erik Schnetter <schnetter@gmail.com>
2014-11-02 23:40:10 -0500

    Simplify program structure

--- Benchmark.jl
+++ Benchmark.jl
@@ -7,47 +7,43 @@ mul1(x) = x * convert(typeof(x), 0.1234)
 
 
 
-function kernel_identity{T}(ni::Int, nj::Int, x::Array{T,1}, y::Array{T,1})
-    for j in 1:nj
-        @simd for i in 1:ni
-            @inbounds y[i] += identity(x[i])
-        end
+function kernel_identity{T}(x::Array{T,1}, y::Array{T,1})
+    @simd for i in 1:length(y)
+        @inbounds y[i] += identity(x[i])
     end
 end
 
-function kernel_add{T}(ni::Int, nj::Int, x::Array{T,1}, y::Array{T,1})
-    for j in 1:nj
-        @simd for i in 1:ni
-            @inbounds y[i] += add1(x[i])
-        end
+function kernel_add{T}(x::Array{T,1}, y::Array{T,1})
+    @simd for i in 1:length(y)
+        @inbounds y[i] += add1(x[i])
     end
 end
 
-function kernel_mul{T}(ni::Int, nj::Int, x::Array{T,1}, y::Array{T,1})
-    for j in 1:nj
-        @simd for i in 1:ni
-            @inbounds y[i] += mul1(x[i])
-        end
+function kernel_mul{T}(x::Array{T,1}, y::Array{T,1})
+    @simd for i in 1:length(y)
+        @inbounds y[i] += mul1(x[i])
     end
 end
 
-function kernel_exp2{T}(ni::Int, nj::Int, x::Array{T,1}, y::Array{T,1})
-    for j in 1:nj
-        @simd for i in 1:ni
-            @inbounds y[i] += exp2(x[i])
-        end
+function kernel_exp2{T}(x::Array{T,1}, y::Array{T,1})
+    @simd for i in 1:length(y)
+        @inbounds y[i] += exp2(x[i])
     end
 end
 
-function kernel_vexp2{T}(ni::Int, nj::Int, x::Array{T,1}, y::Array{T,1})
-    for j in 1:nj
-        @simd for i in 1:ni
-            @inbounds y[i] += vexp2(x[i])
-        end
+function kernel_vexp2{T}(x::Array{T,1}, y::Array{T,1})
+    @simd for i in 1:length(y)
+        @inbounds y[i] += vexp2(x[i])
     end
 end
 
 
+
+function run_benchmark{T}(func, nj::Int, x::Array{T,1}, y::Array{T,1})
+    for j in 1:nj
+        func(x, y)
+    end
+end
 
 function benchmark{T}(func, name, ni::Int, nj::Int, x0::T, dx::T)
     print("Benchmarking $name ($(T)):\n")
@@ -55,9 +51,9 @@ function benchmark{T}(func, name, ni::Int, nj::Int, x0::T, dx::T)
     for i in 1:ni
         x[i] = x0 + i*dx
     end
-    y = similar(x)
-    func(ni, 1, x, y)
-    @time func(ni, nj, x, y)
+    y = zeros(x)
+    run_benchmark(func, 1, x, y)
+    @time run_benchmark(func, nj, x, y)
     s = convert(T, 0.0)
     for i in 1:ni
         s += y[i]
@@ -65,27 +61,22 @@ function benchmark{T}(func, name, ni::Int, nj::Int, x0::T, dx::T)
     return s
 end
 
-
-
-function main()
+function benchmark_type(T::Type)
     ni = 1000
     nj = 1000*1000
-
-    x0 = 1.0
-    dx = 1.0e-9
+    x0 = convert(T, 1.0)
+    dx = convert(T, sqrt(eps(T)))
     benchmark(kernel_identity, "identity", ni,nj,x0,dx)
     benchmark(kernel_add, "add", ni,nj,x0,dx)
     benchmark(kernel_mul, "mul", ni,nj,x0,dx)
     benchmark(kernel_exp2, "exp2", ni,nj,x0,dx)
     benchmark(kernel_vexp2, "vexp2", ni,nj,x0,dx)
+end
 
-    y0 = 1.0f0
-    dy = 1.0f-9
-    benchmark(kernel_identity, "identity", ni,nj,y0,dy)
-    benchmark(kernel_add, "add", ni,nj,y0,dy)
-    benchmark(kernel_mul, "mul", ni,nj,y0,dy)
-    benchmark(kernel_exp2, "exp2", ni,nj,y0,dy)
-    benchmark(kernel_vexp2, "vexp2", ni,nj,y0,dy)
+
+function main()
+    benchmark_type(Float32)
+    benchmark_type(Float64)
 end
 
 # code_native(kernel_identity, (Int, Int, Array{Float64,1}, Array{Float64,1}))
